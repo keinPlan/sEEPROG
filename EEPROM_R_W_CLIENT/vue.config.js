104743cbe2efb6299059003c3957e8e234665b84
keinPlan <4256502+keinPlan@users.noreply.github.com>
2023-01-29 21:12:17 +0100

    Update vue.config.js

--- EEPROM_R_W_CLIENT/vue.config.js
+++ EEPROM_R_W_CLIENT/vue.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  publicPath: process.env.NODE_ENV === 'production' ? '/sEEPROG/' : '/',
   pluginOptions: {
-    publicPath: process.env.NODE_ENV === 'production' ? '/sEEPROG/' : '/',
+
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
     }
