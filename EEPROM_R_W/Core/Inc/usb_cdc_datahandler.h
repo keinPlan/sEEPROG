4992a4bb81ec9f0783a95c01bf9ac61e75af8818
keinPlan <4256502+keinPlan@users.noreply.github.com>
2023-01-29 20:26:24 +0100

    test

---
+++ EEPROM_R_W/Core/Inc/usb_cdc_datahandler.h
@@ -0,0 +1,18 @@
+/*
+ * usb_cdc_datahandler.h
+ *
+ *  Created on: 27.01.2023
+ *      Author: keinplan1337
+ */
+#include "stm32f1xx_hal.h"
+
+#ifndef INC_USB_CDC_DATAHANDLER_H_
+#define INC_USB_CDC_DATAHANDLER_H_
+
+
+
+#endif /* INC_USB_CDC_DATAHANDLER_H_ */
+
+void My_USB_Receive_Handler (uint8_t* Buf, uint16_t Len);
+
+void HandleCommand();
---
+++ EEPROM_R_W_CLIENT/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+
+  data () {
+    return {
+      //
+    }
+  },
+})
+
+
+
+</script>
+
+<style>
+ 
+
+html {
+  font-family: "Ubuntu Mono" !important;
+}
+</style>
